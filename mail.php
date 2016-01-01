3e15cc6e0fa6645d36330e22fe14bcc136c79639
Kamalpreet Chauhan <kmlprtsng@gmail.com>
2016-01-01 12:28:29 +0000

    Customise sending of email via contact form

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
-define('admin_email','your@mail.com'); // Change admin email here for example admin@yoursite.com
-define('website_name','Electrical'); // Change website name here for example yoursite.com
+define('admin_email','info@smartelectricalgb.co.uk'); // Change admin email here for example admin@yoursite.com
+define('website_name','Smart Electrical GB'); // Change website name here for example yoursite.com
 define('website_url', 'http://'.$_SERVER['HTTP_HOST']);
 
 function strict_secure($str){
@@ -17,10 +17,10 @@ function sendEmail($to,$from,$subject,$message,$headname){
 
 if(isset($_POST['action']) && $_POST['action']=='submitform')
 {
-	$N = $_POST['formInput'];	
+	$N = $_POST['formInput'];
 	$path =  $_SERVER['HTTP_REFERER'];
 	$admin_message ='
-	Hello,
+	Hi Amrik,
 	<br /><br /><br /><br />
 	Contact Us Form Received From '.strict_secure($N['name']).'
 	<br /><br />
@@ -29,14 +29,14 @@ if(isset($_POST['action']) && $_POST['action']=='submitform')
 	'.strict_secure($N['message']).'<br /><br />
 	Phone Number:'.strict_secure($N['phone']).'<br /><br />
 	Sender Url:'.$path.'<br /><br /><br /><br />
-	
-	
+
+
 	Regards,<br />
 	'.website_name.'<br />
 	'.website_url.'
 	';
 	$user_message ='
-	Hello '.strict_secure($N['name']).',
+	Hi '.strict_secure($N['name']).',
 	<br /><br /><br /><br />
 	Thank you for contacting us. Your following message has been received by Administrator:
 	<br /><br />
@@ -44,21 +44,24 @@ if(isset($_POST['action']) && $_POST['action']=='submitform')
 	Message:<br /><br />
 	'.strict_secure($N['message']).'<br /><br />
 	Phone Number:'.strict_secure($N['phone']).'<br /><br />
-	
+
 	Regards,<br />
 	'.website_name.'<br />
 	'.website_url.'
 	';
 
-	$sendToAdmin = sendEmail(admin_email,strict_secure($N['email']),'Contact Us Form Received From '.website_name,$admin_message,website_name);
-	$sendToUsers = sendEmail(strict_secure($N['email']),admin_email,'Contact Us Form Sent To '.website_name,$user_message,website_name);
-	
-	
-	if($sendToAdmin && $sendToUsers)
+	$sendToAdmin = sendEmail(admin_email,admin_email,'Contact Us Form Received From '.website_name,$admin_message,website_name);
+	//$sendToUsers = sendEmail(strict_secure($N['email']),admin_email,'Contact Us Form Sent To '.website_name,$user_message,website_name);
+
+
+	if($sendToAdmin) //&& $sendToUsers
 	{
-		$message ='<div class="alert alert-success"><strong>Message Sent Successfully.</strong></div>';		
+		$message ='<div class="alert alert-success"><strong>Message sent successfully.</strong></div>';
 	}else{
-		$message ='<div class="alert alert-danger"><strong>Something Went Wrong While Sending Message.</strong></div>';
+		$message ='<div class="alert alert-danger"><strong>
+								Something went wrong while sending your message.
+								Please contact us directly using our email info@smartelectricalgb.co.uk or telephone 07896535390.
+								</strong></div>';
 	}
 	echo $message;
 }
